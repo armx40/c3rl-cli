85d6f5e9d1d2d135913877f90bb5cc4d1ede85ca
Dipanshu <dipanshu@debian-BULLSEYE-live-builder-AMD64>
2023-03-24 00:57:16 +0530

    Added new sqlite

--- command_data_functions.go
+++ command_data_functions.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/binary"
 	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -159,3 +162,157 @@ func command_data_functions_read_log_main_file(filename string) (main_data pb.Da
 }
 
 /* */
+
+func command_data_process_data_from_file(log_filename string) error {
+
+	// cCtx.Command.Flags
+	/* filename */
+
+	/* open file */
+
+	log_file, err := os.Open(log_filename)
+	if err != nil {
+		return err
+	}
+
+	defer log_file.Close()
+
+	/* prepare tmp buffer */
+
+	buffer := make([]byte, len(command_data_log_file_header))
+
+	/* check header */
+
+	n, err := log_file.Read(buffer)
+	if err != nil {
+		return err
+	}
+
+	if n != len(command_data_log_file_header) {
+		return fmt.Errorf("failed read enough bytes")
+	}
+
+	/* check if header is valid */
+
+	if string(buffer) != command_data_log_file_header {
+		return fmt.Errorf("invalid file header")
+	}
+
+	/* start processing data */
+
+	log_lines := []LogLinePayload{}
+
+	for {
+		line_len_buffer := make([]byte, 4)
+		n, err := log_file.Read(line_len_buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return fmt.Errorf("failed to read line len")
+		}
+		if n != 4 {
+			return fmt.Errorf("failed read enough bytes")
+		}
+
+		/* calculate line line */
+
+		line_len := binary.LittleEndian.Uint32(line_len_buffer)
+
+		if line_len > 1024 {
+			return fmt.Errorf("invalid line len %d", line_len)
+		}
+
+		if line_len == 0 {
+			break
+		}
+
+		/* read entire line */
+		line_buffer := make([]byte, line_len-4)
+		n, err = log_file.Read(line_buffer)
+		if err != nil {
+			return fmt.Errorf("failed to read line")
+
+		}
+
+		if n != int(line_len)-4 {
+			return fmt.Errorf("failed read enough bytes")
+		}
+
+		/* process line options */
+		line_options := binary.LittleEndian.Uint32(line_buffer[0:4])
+
+		/* process line options and check if decryptiong or sign check or hmac is required */
+		is_encrypted := (line_options & DATA_LOG_IS_ENCRYPTED) == 1
+		is_signed := (line_options & DATA_LOG_SIGNED) == 1
+		is_hmaced := (line_options & DATA_LOG_HMACED) == 1
+		/**/
+
+		if is_encrypted {
+
+		}
+
+		if is_hmaced {
+
+		}
+
+		if is_signed {
+
+		}
+
+		/* process line time */
+		line_time := binary.LittleEndian.Uint32(line_buffer[4:8])
+
+		/* process line tag */
+
+		tag_index := 8 + bytes.IndexByte(line_buffer[8:], 0)
+
+		line_tag := string(line_buffer[8:tag_index])
+
+		/* process line code */
+		tag_index += 1
+
+		line_code := binary.LittleEndian.Uint32(line_buffer[tag_index : tag_index+4])
+
+		/* process line */
+
+		line_line := line_buffer[tag_index+4:]
+
+		curr_data := LogLinePayload{
+			LineOptions: line_options,
+			LineTime:    line_time,
+			LineTag:     line_tag,
+			LineLine:    line_line,
+			LineCode:    line_code,
+			LineLength:  line_len,
+		}
+
+		log_lines = append(log_lines, curr_data)
+
+		/* write to local db?? */
+
+		if len(command_data_subcommands_process_sqlite_database_file) > 0 {
+			err = command_data_functions_dump_to_sqlite(command_data_subcommands_process_sqlite_database_file, command_data_subcommands_process_sqlite_database_tablename, &curr_data)
+			if err != nil {
+				return err
+			}
+		}
+
+		/* save to csv file? */
+
+		if len(command_data_subcommands_process_csv_out_csv_file) > 0 {
+			err = command_data_functions_dump_to_csv(command_data_subcommands_process_csv_out_csv_file, []rune(command_data_subcommands_process_csv_out_csv_file_delimiter)[0], &curr_data)
+			if err != nil {
+				return err
+			}
+		}
+
+	}
+
+	if len(command_data_subcommands_process_csv_out_csv_file) > 0 {
+		// close the file
+		command_data_functions_close_csv()
+	}
+
+	return err
+}
--- command_data_subcommands_process_sqlite.go
+++ command_data_subcommands_process_sqlite.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/urfave/cli/v2"
+import (
+	"path/filepath"
+
+	"github.com/urfave/cli/v2"
+)
 
 var command_data_subcommands_process_sqlite_database_file string
 var command_data_subcommands_process_sqlite_database_tablename string
@@ -13,10 +17,17 @@ func command_data_subcommands_process_sqlite_command() (command *cli.Command) {
 		Action:  command_data_subcommands_process_sqlite,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:        "sqlite",
+				Name:        "sqlite-database",
 				Value:       "",
 				Usage:       "create a sqlite database file",
 				Destination: &command_data_subcommands_process_sqlite_database_file,
+				Required:    true,
+			},
+			&cli.IntFlag{
+				Name:        "tail",
+				Value:       1,
+				Usage:       "number of latest data file to process data from",
+				Destination: &command_data_subcommands_process_csv_tail,
 			},
 			&cli.StringFlag{
 				Name:        "sqlite-table",
@@ -29,6 +40,55 @@ func command_data_subcommands_process_sqlite_command() (command *cli.Command) {
 	return command
 }
 
-func command_data_subcommands_process_sqlite(cCtx *cli.Context) error {
+func command_data_subcommands_process_sqlite(cCtx *cli.Context) (err error) {
+
+	/* ask for storage device */
+	user_device, err := command_devices_functions_read_user_storage_device()
+	if err != nil {
+		return
+	}
+	/* */
+
+	/* get the main data log file settings */
+	log_settings, err := command_data_functions_read_log_main_file(filepath.Join(user_device.MountPoint, "data_log_main.data"))
+	if err != nil {
+		return
+	}
+	/* */
+
+	/* get the latest file index */
+	latest_file_index := log_settings.FileCounter % 40000
+	/* */
+
+	/* calculate how many files needs to be read */
+
+	number_of_files_to_read := command_data_subcommands_process_csv_tail
+
+	/* */
+
+	/* find of many data logs files are present */
+	log_files, err := command_devices_functions_get_all_log_files_sorted(user_device, int(latest_file_index), number_of_files_to_read)
+	if err != nil {
+		return
+	}
+
+	/* */
+
+	/* process each file */
+
+	/* */
+	for i := range log_files {
+		err = command_data_process_data_from_file(filepath.Join(user_device.MountPoint, log_files[i].Name()))
+		if err != nil {
+			return err
+		}
+	}
+
+	/* close sqlite here */
+	if len(command_data_subcommands_process_sqlite_database_file) > 0 {
+		// close the file
+		command_data_functions_close_sqlite()
+	}
+	/**/
 	return nil
 }
